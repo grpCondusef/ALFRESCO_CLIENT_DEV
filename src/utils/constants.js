58617b2421412332361477d10d61016ca9a81c16
grpCondusef <grpcondusef@gmail.com>
2025-04-09 13:26:13 -0600

    test github

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+
+//URL DEL BACKEND DE LA API
+//export const URL_API = `http://10.33.1.238:81`
+export const URL_API = `http://10.33.200.115:81`
+//export const URL_API = `http://localhost:5173/`
+
+//ESPACIO DEL LOCAL STORAGE
+export const CONDUSEF_TOKEN = "CONDUSEF_TOKEN"
